f877afa81e00cc54e72934eb08a30066e3b559e5
harsh082ip <harshhvstech1975@gmail.com>
2024-05-20 22:47:32 +0530

    Added some code

---
+++ basic/connect.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/redis/go-redis/v9"
+
+func RedisConnect() *redis.Client {
+	url := "redis://localhost:6379"
+	opts, err := redis.ParseURL(url)
+	if err != nil {
+		panic(err)
+	}
+
+	return redis.NewClient(opts)
+}
--- basic/main.go
+++ basic/main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
-
-	"github.com/go-redis/redis/v8"
 )
 
 func main() {
 	ctx := context.Background()
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379", // Redis address
-		Password: "",               // No password set
-		DB:       0,                // Use default DB
-	})
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379", // Redis address
+	// 	Password: "",               // No password set
+	// 	DB:       0,                // Use default DB
+	// })
+	// opt, err := redis.ParseURL("redis://localhost:6379")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// rdb := redis.NewClient(opt)
+
+	rdb := RedisConnect()
+	// fmt.Println(rdb)
 
 	// Ping the Redis server
 	pong, err := rdb.Ping(ctx).Result()
@@ -21,4 +28,23 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(pong)
+
+	val, _ := rdb.Set(ctx, "user:1", "harsh", 0).Result()
+	fmt.Println(val)
+	// fmt.Println(rdb)
+
+	res, err := rdb.Del(ctx, "user:1").Result()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(res)
+
+	success, err := rdb.SetNX(ctx, "user:1", "ankiit", 0).Result()
+	if err != nil {
+		fmt.Println("error")
+		fmt.Println(success, err.Error())
+	}
+	fmt.Println(success)
+	val, _ = rdb.Get(ctx, "name").Result()
+	fmt.Println(val)
 }
